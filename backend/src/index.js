dd71f1f71db163435830208e3c7f28d2f06b2230
Anh-Tuan1412 <maianhtuan06022005@gmail.com>
2026-04-07 17:18:26 +0700

    Initial commit: Upload project folder

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+const app = express();
+app.use('api/route', authRoutes);
+
+app.listen(5001, () => {
+    console.log("hello")
+})
