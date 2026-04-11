a5f1f555fbe0069e69fb5e6e68c097dc2f8a1c6e
Anh-Tuan1412 <maianhtuan06022005@gmail.com>
2026-04-11 18:12:19 +0700

    feat(Backend): Implement Login Logout and updateProfile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/util.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
    
@@ -44,13 +45,72 @@ export const signup = async (req, res) => {
             res.status(400).json({message:"Dữ liệu người dùng không hợp lệ"})
         }
    }catch(error){
-    console.log("Có lỗi trong đăng ký", error.message);
+    console.log("Error in signup controller", error.message);
     res.status(500).json({message: "Internal Server Error"});
    }
 }
 
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email})
 
+        if(!user) return res.status(404).json({message:"Email này chưa được đăng ký"})
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect) {
+            return res.status(400).json({message:"Mật khẩu không hợp lệ"})
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+        });
+    }catch(error){
+        console.log("Error in login controller", error.message);
+
+        res.status(500).json({message: "Internal server error"});
+    }
+}
 
 export const logout = (req, res) => {
-    res.send('logout route');
+    try{
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfully"});
+
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message: "Cần cung cấp hình ảnh đại diện"})
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profiflePic: uploadResponse.secure_url}, {new: true});
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("error in update profile", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
 }
