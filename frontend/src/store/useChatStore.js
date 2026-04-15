9048270991b999f6ca6506d6288793197ab395b4
Anh-Tuan1412 <maianhtuan06022005@gmail.com>
2026-04-15 22:39:23 +0700

    feat(frontend): integrate socket client for online users and realtime messages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios.js";
+import { useAuthStore } from "./useAuthStore.js";
 
 export const useChatStore = create((set, get) => ({
   messages: [],
@@ -47,6 +48,25 @@ export const useChatStore = create((set, get) => ({
       toast.error(error.response.data.message);
     }
   },
-  // code sau
+
+  subscribeToMessages: () => {
+    const{selectedUser} = get();
+    if(!selectedUser) return;
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => { 
+        if (newMessage.senderId !== selectedUser._id) return;
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+  unSubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage"); 
+
+  },
+  
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
